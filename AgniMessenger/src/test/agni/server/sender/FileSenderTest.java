8a7bfe5f18c09cc880901dcf0e2198ce826b924b
Enoch Tsang <etsang1@hotmail.com>
2016-03-14 12:54:43 -0600

    added files for other server sender tests

---
+++ AgniMessenger/src/test/agni/server/sender/FileSenderTest.java
@@ -0,0 +1,14 @@
+package test.agni.server.sender;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileSenderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ AgniMessenger/src/test/agni/server/sender/InfoSenderTest.java
@@ -0,0 +1,14 @@
+package test.agni.server.sender;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InfoSenderTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
