12eaa13f92cf3f6f8d0894082ac3d81dbc343af7
Enoch Tsang <etsang1@hotmail.com>
2016-03-14 12:33:28 -0600

    initial

---
+++ AgniMessenger/src/test/agni/client/action/ChatActionHandlerTest.java
@@ -0,0 +1,14 @@
+package test.agni.client.action;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChatActionHandlerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
