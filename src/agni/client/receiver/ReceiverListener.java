2b84bcb1d99226fdfcbcaaff2807a5e68d5af9d5
mfalthaw <Muaz@10.12.133.184>
2016-03-19 21:17:48 -0600

    ReceiverListener's methods

--- src/agni/client/receiver/ReceiverListener.java
+++ src/agni/client/receiver/ReceiverListener.java
@@ -1,5 +1,13 @@
 package agni.client.receiver;
 
+import agni.server.sender.StatusSender.Status;
+
 public interface ReceiverListener {
+	
+	public void heartbeatReaction();
+	public void infoReaction(String message);
+	public void statusReaction(String user, Status status);
+	public void chatReaction(String sender, String message);
+	public boolean fileReaction(String filename, String fromUser, int size);
 
 }
--- src/agni/client/view/ChatView.java
+++ src/agni/client/view/ChatView.java
@@ -1,6 +1,7 @@
 package agni.client.view;
 
 import agni.client.action.*;
+import agni.server.sender.StatusSender.Status;
 
 public class ChatView implements AgniClientView {
 
@@ -22,6 +23,36 @@ public class ChatView implements AgniClientView {
         this.fileActionHandler = fileActionHandler;
         this.heartbeatActionHandler = heartbeatActionHandler;
     }
+
+	@Override
+	public void heartbeatReaction() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void infoReaction(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void statusReaction(String user, Status status) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void chatReaction(String sender, String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean fileReaction(String filename, String fromUser, int size) {
+		// TODO Auto-generated method stub
+		return false;
+	}
     
     
 }
