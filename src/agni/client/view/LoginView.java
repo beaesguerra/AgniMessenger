a17b5abb02e1eff47839faaca742c9b0643849ec
beaesguerra <bea.esguerra@Hotmail.com>
2016-03-20 17:47:57 -0600

    added run stubs to client views

--- src/agni/client/view/LoginView.java
+++ src/agni/client/view/LoginView.java
@@ -16,4 +16,10 @@ public class LoginView implements AgniClientView {
         this.infoRequestActionHandler = infoRequestActionHandler;
         this.heartbeatActionHandler = heartbeatActionHandler;
     }
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 }
