9f63b19a69284cb6d1e443b7afd173f0d5519e7a
Enoch Tsang <etsang1@hotmail.com>
2016-03-22 11:19:00 -0600

    stubs for dataguard tests

---
+++ src/test/agni/server/dataguard/GroupChatDataGuardTest.java
@@ -0,0 +1,14 @@
+package test.agni.server.dataguard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GroupChatDataGuardTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
