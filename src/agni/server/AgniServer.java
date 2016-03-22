37686a5312e7ba9d185844f9fb88917763e06e17
beaesguerra <bea.esguerra@Hotmail.com>
2016-03-22 10:32:50 -0600

    made IPChannelPair to pair ip addresses and channels. added an array of pairs in agniserver

--- src/agni/server/AgniServer.java
+++ src/agni/server/AgniServer.java
@@ -1,6 +1,10 @@
 package agni.server;
 
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+
 public class AgniServer {
+	ArrayList<IPChannelPair<String,SocketChannel>> channels; 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ src/agni/server/IPChannelPair.java
@@ -0,0 +1,10 @@
+package agni.server;
+
+public class IPChannelPair<I,C> {
+	private final I ipAddress;
+	private final C channel; 
+	public IPChannelPair(I ip, C channel) {
+		this.ipAddress = ip;
+		this.channel = channel; 
+	}
+}
