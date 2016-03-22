96977114e7b361882a67b0f47e0986cc7b317b27
jeinglis <jeinglis@ucalgary.ca>
2016-03-22 10:24:53 -0600

    corrected initialize method

--- src/agni/server/communication/MessageReceiver.java
+++ src/agni/server/communication/MessageReceiver.java
@@ -1,36 +1,31 @@
 package agni.server.communication;
 
-public class MessageReceiver implements Runnable {
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+
+public class MessageReceiver {
 
     private Selector selector = null;
     private ServerSocketChannel channel = null;
-
-
-    public static void main(String[] args) {
-
-        initializeConnection (args[0]);
-
-        //TODO instantiate receivers
-        
-    }
 
 /*
 *@requires server port number
 *@promises selector initialized and ready for connection requests
 *@promises socket channel is initialized to be non-blocking and bound to specified port
 */
-    private void initializeConnection(String port) {
+    public void initializeConnection(String port) {
 
-        channel. ServerSocketChannel.open();
+        ServerSocketChannel.open();
         channel.configureBlocking(false);
-        InetSocketAddress isa  new InetSocketAddress (Integer.parseInt(port));
+        InetSocketAddress isa = new InetSocketAddress (Integer.parseInt(port));
         channel.socket().bind(isa);
 
         selector = Selector.open();
         channel.register(selector, SelectionKey.OP_ACCEPT);
     }
 
-    public getSelector
 
 }
 
