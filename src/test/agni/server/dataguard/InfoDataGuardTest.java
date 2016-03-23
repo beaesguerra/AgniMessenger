21f616ea9c6e961db4556db00a83f4e301b2fd04
Enoch Tsang <etsang1@hotmail.com>
2016-03-22 23:33:46 -0600

    info dataguard tests

--- src/test/agni/server/dataguard/InfoDataGuardTest.java
+++ src/test/agni/server/dataguard/InfoDataGuardTest.java
@@ -6,23 +6,35 @@ import org.junit.Test;
 
 public class InfoDataGuardTest {
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
+    private InfoDataGuard infoDataGuard;
+
+    @Before
+    public void setup() {
+        infoDataGuard = new InfoDataGuard('agni_test', 'test', '');
     }
 
     @Test
     public String serverNameNotNullTest() {
-        fail("Not yet implemented");
+        assertThat(infoDataGuard.serverName(), not(null));
     }
 
     @Test
     public int currentOnlineTest() {
-        fail("Not yet implemented");
+        assertEquals(infoDataGuard.currentOnline(), 0);
+        
+        infoDataGuard.incrementUsersOnline();
+        assertEquals(infoDataGuard.currentOnline(), 1);
+        
+        infoDataGuard.incrementUsersOnline();
+        assertEquals(infoDataGuard.currentOnline(), 2);
+        
+        infoDataGuard.decerementUsersOnline();
+        infoDataGuard.decerementUsersOnline();
+        assertEquals(infoDataGuard.currentOnline(), 0);
     }
 
     @Test
-    public int totalUsers() {
-        fail("Not yet implemented");
+    public int totalUsersTest() {
+        assertEquals(infoDataGuard.totalUsers(), 0);
     }
 }
