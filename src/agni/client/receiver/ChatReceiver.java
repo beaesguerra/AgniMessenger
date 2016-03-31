2a88e178ce1ecc4c144a7fcc0764c0a5de8d951f
mfalthaw <Muaz@10.13.133.107>
2016-03-31 17:16:28 -0600

    fixed zero-starting index

--- src/agni/client/receiver/ChatReceiver.java
+++ src/agni/client/receiver/ChatReceiver.java
@@ -18,11 +18,13 @@ public class ChatReceiver extends MessageParser {
 	public void receiveMessage(byte[] message) {
 		if(message == null)
 			throw new NullPointerException("receiveMessage got a null message");
-		// Sender's name -> 6th bit [5]
-		int senderNameLength = (int) message[5];
-		byte[] parsedMessage = Arrays.copyOfRange(message, 7+senderNameLength, message.length);
+		// Sender's name length -> 5th bit [4]
+		int senderNameLength = (int) message[4];
+		// Message starts -> 7th bit [6]
+		byte[] parsedMessage = Arrays.copyOfRange(message, 6+senderNameLength, message.length);
 		try {
-			String src = new String(Arrays.copyOfRange(message, 6, 6+senderNameLength), "us-ascii");
+			// Sender's name starts -> 6th bit [5]
+			String src = new String(Arrays.copyOfRange(message, 5, 5+senderNameLength), "us-ascii");
 			notifyChatReceived(src, new String(parsedMessage, "us-ascii"));
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
