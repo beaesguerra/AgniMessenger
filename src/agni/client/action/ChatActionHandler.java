a0e95a063b8f0b15705e24fa2e8ab34c09d27f8b
mfalthaw <Muaz@10.13.133.107>
2016-03-31 23:13:50 -0600

    starting chatHandler

--- src/agni/client/action/ChatActionHandler.java
+++ src/agni/client/action/ChatActionHandler.java
@@ -4,12 +4,27 @@ import agni.client.communication.MessageSender;
 
 public class ChatActionHandler {
     private MessageSender messageSender;
+    final private int HEADER_LENGTH_SIZE = 4;
+    final private int MESSAGE_TYPE_SIZE = 1;
+    final private byte MESSAGE_TYPE = 0x05;
 
     public ChatActionHandler(MessageSender messageSender) {
         this.messageSender = messageSender;
     }
 
     public void requestChatAction(String message) {
-
+    	
+    }
+    
+    private static boolean isAllAscii(String input) {
+        boolean isAscii = true;
+        for (int i = 0; i < input.length(); i++) {
+            int c = input.charAt(i);
+            if (c > 0x7F) {
+                isAscii = false;
+                break;
+            }
+        }
+        return isAscii;
     }
 }
--- src/agni/client/action/HeartbeatActionHandler.java
+++ src/agni/client/action/HeartbeatActionHandler.java
@@ -10,7 +10,7 @@ public class HeartbeatActionHandler {
     }
 
     public void sendHeartbeat(byte status) {
-
+    	
     }
 
 }
