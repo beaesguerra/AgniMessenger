a91d93a61cc9228e519ce6ec01b7dc05807597f9
mfalthaw@ucalgary.ca <mfalthaw@ucalgary.ca>
2016-04-01 15:44:40 -0600

    intToByteArray

--- src/agni/client/action/LoginActionHandler.java
+++ src/agni/client/action/LoginActionHandler.java
@@ -12,5 +12,13 @@ public class LoginActionHandler {
     public void requestLogin(String username, String password) {
 
     }
-
+    
+    public static final byte[] intToByteArray(int value) {
+        return new byte[] {
+                   (byte)(value >>> 24),
+                   (byte)(value >>> 16),
+                   (byte)(value >>> 8),
+                   (byte)value
+               };
+    }
 }
