516efa454bb8b77fef319af84f2abf1486398b31
jeinglis <jeinglis@ucalgary.ca>
2016-04-01 20:18:58 -0600

    finished the sender logic

--- src/agni/client/communication/MessageSender.java
+++ src/agni/client/communication/MessageSender.java
@@ -1,16 +1,35 @@
 package agni.client.communication;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class MessageSender {
 
     private Socket tcpSocket;
+    private DataOutputStream dataOut = null;
 
-    public MessageSender() {
-
+    public MessageSender(Socket clientSocket) {
+    	tcpSocket = clientSocket;
+    	OutputStream out;
+		try {
+			out = tcpSocket.getOutputStream();
+	    	dataOut = new DataOutputStream(out);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     public void sendMessage(byte[] message) {
-
+    	int length = message.length;
+    	try {
+			dataOut.write(message, 0, length);
+			dataOut.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
