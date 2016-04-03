01b113a1efb16e28e75812995bbd005efa096e55
Bea <bea.esguerra@hotmail.com>
2016-04-03 01:40:41 -0600

    status change to offline implementation

--- src/agni/server/manager/StatusManager.java
+++ src/agni/server/manager/StatusManager.java
@@ -4,10 +4,12 @@ import agni.server.dataguard.I_UserDataGuard;
 import agni.server.dataguard.UserDataGuard;
 import agni.server.receiver.StatusListener;
 import agni.server.sender.StatusSender;
+import agni.server.sender.StatusSender.Status;
 
 public class StatusManager implements StatusListener{
     private StatusSender statusSender;
     private I_UserDataGuard userDataGuard;
+
 
     public StatusManager(StatusSender statusSender,
                          UserDataGuard userDataGuard) {
@@ -17,7 +19,16 @@ public class StatusManager implements StatusListener{
 
     @Override
     public void receiveStatusChange(String ip, byte status) {
-        // TODO Auto-generated method stub
+        String username = userDataGuard.getUsername(ip); 
+    	for (String friend : userDataGuard.getFriends(username)) {
+    		if (status == Status.OFFLINE.bytes()) {
+    			 statusSender.sendStatus(userDataGuard.userCurrentIp(friend), Status.OFFLINE, username);  
+            }
+            else if (status == Status.ONLINE.bytes()) { 
+            	
+            }
+    	}
+        
         
     }
 }
