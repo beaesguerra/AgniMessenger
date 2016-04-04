86a4ab3b03f0d407ce8b5de8fef203a7a970e0b2
Enoch Tsang <etsang1@hotmail.com>
2016-04-04 05:34:33 -0600

    server send new lines

--- src/agni/server/communication/MessageSender.java
+++ src/agni/server/communication/MessageSender.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 
 public class MessageSender implements I_MessageSender {
@@ -21,12 +22,18 @@ public class MessageSender implements I_MessageSender {
         System.out.println(" DONE SENDING BYTE ARRAY");
         // System.out.println(new String(message, StandardCharsets.US_ASCII));
         SocketChannel currentChannel = channelList.getChannel(ipAddress);
-        ByteBuffer outBuffer = ByteBuffer.wrap(message);
+        ByteBuffer outBuffer = ByteBuffer.wrap(addNewline(message));
         try {
             currentChannel.write(outBuffer);
         } catch (IOException e) {
             System.out.println("IOException caught while writing to Client MessageSender outBuffer ");
             e.printStackTrace();
         }
+    }
+    
+    byte[] addNewline(byte[] message) {
+        byte[] result = Arrays.copyOf(message, message.length + 1);
+        result[message.length] = (byte)0x0A;
+        return result;
     }
 }
