789f4e0c60bec0ae1536ae32ff8d195070cc0ead
Bea <bea.esguerra@hotmail.com>
2016-04-03 19:06:31 -0600

    changed tabs to spaces and removed extra lines

--- src/agni/server/dataguard/I_InfoDataGuard.java
+++ src/agni/server/dataguard/I_InfoDataGuard.java
@@ -1,18 +1,11 @@
 package agni.server.dataguard;
 
 public interface I_InfoDataGuard {
-
     public String serverName();
-
     public int currentOnline();
-
     public void incrementUsersOnline();
-
     public void decrementUsersOnline();
-
     public int totalUsers();
-    
     public String [] usersOnline();
-
-	public String [] availableChats(); 
+    public String [] availableChats(); 
 }
